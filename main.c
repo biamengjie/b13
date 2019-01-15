c6c5ebd76226721b4af49a906c9ac3453042f60c
Your Name <you@example.com>
2019-01-15 19:25:17 +0800

    构建应用基本框架

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("jinruminganzifuguanlixitong:\n");
+    while (1)
+    {
+        printf("1---tijiayigemingan:\n");
+        printf("2---shanchuzuihouyigeminganzifuminganzifu:\n");
+        printf("3---xianshiyitianjiademinganzifu:\n");
+        printf("4---tihuanminganzifu:\n");
+        printf("5---tuichu:\n");
+        printf("qingshuruxiangdegongnengbianhao:\n");
+        int a;
+        scanf("%d",&a);
+        
+        if(a==1){
+            
+        }
+        if(a==2){
+            
+        }
+        if(a==3){
+            
+        }
+        if(a==4){
+            
+        }
+        if(a==5){
+            
+        }
+    }
 
     return 0;
-
 }
